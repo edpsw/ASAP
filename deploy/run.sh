13d0e7dfc5492fe596353970139c831c15713c17
edpsw <1589152167@qq.com>
2025-04-21 08:35:22 +0800

    sh

---
+++ deploy/run.sh
@@ -0,0 +1,8 @@
+python deploy/deploy_mujoco/deploy_mujoco.py g1.yaml
+python deploy/deploy_mujoco/deploy_mujoco_pt.py g1.yaml
+python deploy/deploy_mujoco/deploy_mujoco_onxx.py g1_21.yaml
+
+python deploy/deploy_mujoco/deploy_g1_29_23.py 
+python deploy/deploy_mujoco/deploy_g1_23_21.py 
+
+python deploy/deploy_real/deploy_real.py enp3s0 g1.yaml
